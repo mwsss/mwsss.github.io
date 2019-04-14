c1fd9803f9c01df560e320c977745b4c9383f614
mwsss <inatonij@gmail.com>
2019-04-15 03:35:39 +0900

    rnd fix
    chr rnd fix

--- puchi11k02/e212p02-HTML5-Shipping.UE4.js
+++ puchi11k02/e212p02-HTML5-Shipping.UE4.js
@@ -68,7 +68,7 @@ var Module = {
 	assetDownloadProgress: {}, // Track how many bytes of each needed asset has been downloaded so far.
 
 	UE4_indexedDBName: 'UE4_assetDatabase_e212p02', // this should be an ascii ID string without special characters that is unique to the project that is being packaged
-	UE4_indexedDBVersion: 201904141641, // Bump this number to invalidate existing IDB storages in browsers.
+	UE4_indexedDBVersion: 201904141816, // Bump this number to invalidate existing IDB storages in browsers.
 };
 
 
